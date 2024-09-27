42b6fae7688c477999a5d08a498a5cddb191eac5
eric.ruiz.7e6 <eric.ruiz.7e6@itb.cat>
2024-09-27 08:46:32 +0200

    TA02

---
+++ TA02/comprovarEdat.py
@@ -0,0 +1,14 @@
+"""
+Eric Ruiz Fernandez
+27/9/2024
+ASIXcB MDS TA02
+Descripció: Exemple dels apunts
+"""
+
+
+#Programa que demana la edat i diu si ets major d'edat.
+
+edat=int(input("Quina edat tens?"))
+if edat>=18:
+    print("Ets major d'edat")
+print("Programa Finalitzat")
---
+++ TA02/helloworld.py
@@ -0,0 +1,14 @@
+"""
+Eric Ruiz Fernandez
+27/9/2024
+ASIXcB MDS TA02
+Descripció:
+Estructura d'un programa informàtic - Algoritmes - Git
+Bla Bla Bla...
+Bla Bla Bla...
+Bla Bla Bla...
+"""
+
+
+print ("Hola món ;-)")
+
