b24e6947e2170713782afeeba20296ab83258b18
eric.ruiz.7e6 <eric.ruiz.7e6@itb.cat>
2024-10-08 10:42:08 +0200

    TA02

--- TA02/comprovarEdat.py
+++ TA02/comprovarEdat.py
@@ -8,6 +8,25 @@ Descripció: Exemple dels apunts
 
 #Programa que demana la edat i diu si ets major d'edat.
 
+AquestAny = 2024
+
+dia = int(input("Quin dia vas neixer?"))
+mes =int(input("Quin mes vas neixer?"))
+any int(input("Quin any vas neixer?"))
+
+if dia > 31:
+    print("dia no valid")
+
+if mes > 12:
+    print("mes no valid")
+
+if any > 2024:
+    print("mes no valid")
+
+
+
+
+'''
 edat=int(input("Quina edat tens?"))
 if edat>=18:
     print("Ets major d'edat")
@@ -15,3 +34,4 @@ print("Programa Finalitzat")
 
 
 #PROVA COMMIT
+'''
