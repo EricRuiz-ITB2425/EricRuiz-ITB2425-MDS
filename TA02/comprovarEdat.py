b029c3e0eb817786d5c65c14f31cce314f4a2c83
eric.ruiz.7e6 <eric.ruiz.7e6@itb.cat>
2024-10-15 08:53:18 +0200

    TA02

--- TA02/comprovarEdat.py
+++ TA02/comprovarEdat.py
@@ -18,43 +18,20 @@ dia = int(input("Quin dia vas neixer?"))
 mes = int(input("Quin mes vas neixer?"))
 any = int(input("Quin any vas neixer?"))
 
-
-while True:
-    if dia > 31:
-        print("dia no valid")
-    elif dia > 31:
-        print("Dia no valid")
-        break
-
-    if mes > 12:
-        print("mes no valid")
-    elif mes > 12:
-        print("Mes no valid")
-        break
-
-    if any > 2024:
-        print("mes no valid")
-        producte: int = any-AquestAny
-    elif any > 2024:
+try:
+    if any <= AquestAny:
+        if mes >= 1 and mes <=12:
+            if dia >= 1 and dia <= 31:
+                print("Calculant...")
+                edat = AquestAny-any
+                if edat >= 16 and edat <= 65:
+                    print("Pots treballar.")
+                else:
+                    print("No pots treballar.")
+    else:
         print("Any no valid")
-        break
 
-any - AquestAny = edat
-
-
-print(edat)
-
-
-
-
-
-
-'''
-edat=int(input("Quina edat tens?"))
-if edat>=18:
-    print("Ets major d'edat")
-print("Programa Finalitzat")
-
+except:
+    print("Error")
 
 #PROVA COMMIT
-'''
