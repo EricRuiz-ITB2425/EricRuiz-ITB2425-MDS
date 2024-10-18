96cfc0b3b293e5f19e739f4452742ff637a25ff7
Eric <eric.ruiz.7e6@itb.cat>
2024-10-18 12:23:20 +0200

    a

---
+++ T03/SCRIPT.py
@@ -0,0 +1,9 @@
+"""
+Eric Ruiz Fernandez, Manuel Reyes Chinga, Ernesto Martinez Argueta, Izan Fernandez Arrea.
+18/10/2024
+ASIXcB MDS TA03
+Descripció: Script Processar les dades
+"""
+
+holas
+pipiip
